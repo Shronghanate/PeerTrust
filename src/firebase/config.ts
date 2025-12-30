0d12530193d02a7287178119b0f6650de10c5458
SHRON GHANATE <shronghanate@gmail.com>
2025-12-30 08:23:31 +0000

    can it run on vercel without any error

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,7 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-3134818675-cb56d",
-  "appId": "1:187733146494:web:695659da8a2ca67fb3912f",
-  "apiKey": "AIzaSyDICiGFg5ShvBchvuY_Z0JgivgFjALvFsY",
-  "authDomain": "studio-3134818675-cb56d.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "187733146494"
+  // This object is intentionally left empty.
+  // Firebase App Hosting provides the configuration automatically
+  // when initializeApp() is called without arguments in its environment.
+  // For local development, you would populate this with your Firebase project's web app config.
 };
