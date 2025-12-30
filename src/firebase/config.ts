a898953662dba215f8a4023a83ff2dce1f13ffaf
SHRON GHANATE <shronghanate@gmail.com>
2025-12-30 06:34:13 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3134818675-cb56d",
+  "appId": "1:187733146494:web:695659da8a2ca67fb3912f",
+  "apiKey": "AIzaSyDICiGFg5ShvBchvuY_Z0JgivgFjALvFsY",
+  "authDomain": "studio-3134818675-cb56d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "187733146494"
+};
